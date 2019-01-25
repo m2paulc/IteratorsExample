872ae8340e293e98d52b39536398df7e00970006
Paul C <hyp3rsp8c3@gmail.com>
2019-01-25 22:50:36 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//Iterator Example
+function nameIterator(names) {
+  let nextIndex = 0;
+
+  return {
+    next: function() {
+      return nextIndex < names.length ? { value: names[nextIndex++], done: false } : { done: true }
+    }
+  }
+}
